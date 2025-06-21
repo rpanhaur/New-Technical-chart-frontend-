1df473586832b4302f2136f44fd9bea83e1b3f3f
rpanhaur <rpanhaur@gmail.com>
2025-06-21 11:23:14 +0545

    signup and login set

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,32 +1,33 @@
 import { Link } from "react-router-dom"
 
-const Navbar=()=>{
-  return(
+const Navbar = () => {
+  return (
     <>
 
-{/* Navbar */}
-<nav className="bg-white shadow-lg">
-  <div className="max-w-7xl mx-auto px-4">
-    <div className="flex justify-between items-center py-4">
-      {/* Logo */}
-      <div className="text-2xl font-bold text-indigo-600">
-        Technical Chart
-      </div>
-      {/* Desktop Menu */}
-      <div className="hidden md:flex space-x-6 text-gray-700 font-medium">
-      <Link to="/login" className="hover:text-indigo-600 transition duration-200">Login</Link>
-        <Link to="/" className="hover:text-indigo-600 transition duration-200">Home</Link>
-        <Link to="/create-duty" className="hover:text-indigo-600 transition duration-200">Create Duty Schedule </Link>
-        
-        <a href="#" className="hover:text-indigo-600 transition duration-200">Edit Technical Chart</a>
-        <Link to="/create-page" className="hover:text-indigo-600 transition duration-200">Create Technical Chart</Link>
-      </div>
-    </div>
-  </div>
-</nav>
+      {/* Navbar */}
+      <nav className="bg-white shadow-lg">
+        <div className="max-w-7xl mx-auto px-4">
+          <div className="flex justify-between items-center py-4">
+            {/* Logo */}
+            <div className="text-2xl font-bold text-indigo-600">
+              Technical Chart
+            </div>
+            {/* Desktop Menu */}
+            <div className="hidden md:flex space-x-6 text-gray-700 font-medium">
+              <Link to="/login" className="hover:text-indigo-600 transition duration-200">Login</Link>
+              <Link to="/" className="hover:text-indigo-600 transition duration-200">Home</Link>
+              <Link to="/create-duty" className="hover:text-indigo-600 transition duration-200">Create Duty Schedule </Link>
 
-    
-    
+              <a href="#" className="hover:text-indigo-600 transition duration-200">Edit Technical Chart</a>
+              <Link to="/create-page" className="hover:text-indigo-600 transition duration-200">Create Technical Chart</Link>
+              <Link to="/sign-up" className="hover:text-indigo-600 transition duration-200">SignUp</Link>
+            </div>
+          </div>
+        </div>
+      </nav>
+
+
+
     </>
   )
 }
